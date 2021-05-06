a0950b2d7318f0c34f43b18cbed60d975bbc3f74
Nagarjuna Yadav K <nagarjunayadavk@gmail.com>
2021-05-06 18:38:41 +0530

    React Redux Add/Remove Using Single state

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { GET_POSTS, GET_POSTS_FAILURE, GET_POSTS_SUCCESS } from "./actionTypes";
+import { ADD_POST_FAILURE, ADD_POST_INIT, ADD_POST_SUCCESS, 
+    DELETE_POST_FAILURE, 
+    DELETE_POST_INIT, 
+    DELETE_POST_SUCCESS, 
+    GET_POSTS, GET_POSTS_FAILURE, GET_POSTS_SUCCESS } from "./actionTypes";
 
 
 export const getPostsInit = () => ({
@@ -28,6 +32,70 @@ export function getPosts() {
             .catch(error => {
                 console.log(error);
                 dispatch(getPostsFailure(error.toString()));
+            })
+    }
+    
+}
+
+
+export const addPostInit = () => ({
+    type: ADD_POST_INIT
+});
+export const addPostSuccess = (post) => ({
+    type: ADD_POST_SUCCESS,
+    post
+});
+export const addPostFailure = (error) => ({
+    type: ADD_POST_FAILURE,
+    error
+});
+
+export function addPost(post) {
+    return dispatch =>{
+        dispatch(addPostInit());
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/posts',
+            data: post
+        })
+            .then(response => {
+                console.log(response.data);
+                dispatch(addPostSuccess(response.data));
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch(addPostFailure(error.toString()));
+            })
+    }
+    
+}
+
+export const deletePostInit = () => ({
+    type: DELETE_POST_INIT
+});
+export const deletePostSuccess = (postid) => ({
+    type: DELETE_POST_SUCCESS,
+    postid
+});
+export const deletePostFailure = (error) => ({
+    type: DELETE_POST_FAILURE,
+    error
+});
+
+export function deletePost(postId) {
+    return dispatch =>{
+        dispatch(deletePostInit());
+        axios({
+            method: 'delete',
+            url: 'http://localhost:5000/posts/' + postId
+        })
+            .then(response => {
+                console.log(response.data);
+                dispatch(deletePostSuccess(postId));
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch(deletePostFailure(error.toString()));
             })
     }
     
